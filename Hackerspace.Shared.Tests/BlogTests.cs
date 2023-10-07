c08948eda1dcab5d32ed4777506614d5507548d3
Haley6548 <hvt6548@gmail.com>
2023-10-06 20:20:52 -0600

    signing off

---
+++ Hackerspace.Shared.Tests/BlogTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerspace.Shared.Tests
+{
+    [TestClass]
+    public class BlogTests
+    {
+    //    [TestMethod]
+    //    public void PostClassExists()
+    //    {
+    //        var classUnderTest = new Post();
+    //        classUnderTest.Id = 1;
+    //        classUnderTest.Text = "test";
+    //        var testDate = DateTime.Now
+    //        classUnderTest.Date = testDate;
+    //        Assert.
+    //    }
+    }
+}
