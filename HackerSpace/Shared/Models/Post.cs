670c3cb9d07ecd60d55f9c626bba263f2fad5e73
Haley6548 <hvt6548@gmail.com>
2023-10-19 11:56:37 -0600

    added post repo interface

--- HackerSpace/Shared/Models/Post.cs
+++ HackerSpace/Shared/Models/Post.cs
@@ -9,9 +9,9 @@ namespace HackerSpace.Shared.Modles
     public class Post
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Text { get; set; }
-        public DateTime Date { get; set; }
+        public string Title { get; set; } = "";
+        public string Text { get; set; } = "";
+        public DateTime Date { get; set; } = DateTime.Now;
 
     }
 }
